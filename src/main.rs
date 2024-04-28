8d5c1e2089d3c4c4b0c447428e5335f39bfefbb6
Sandro Pischinger <mail@sandropischinger.de>
2024-04-28 15:03:49 +0200

    pack it all in one binary

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use actix_web::{middleware::Logger, get, web, App, Error, HttpServer, Responder, HttpResponse, HttpRequest};
-use actix_files::Files;
-use askama::Template;
+use actix_web::{middleware::Logger, get, web, App, Error, HttpServer,  HttpResponse, HttpRequest};
 use env_logger;
 use awc::Client;
+use actix_web_static_files::ResourceFiles;
 
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate {}
-
-#[get("/")]
-async fn index() -> impl Responder {
-    IndexTemplate{}
-}
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 #[get("/stream/0")]
 async fn proxy(
@@ -35,19 +27,51 @@ async fn proxy(
   Ok(client_resp.streaming(res))
 }
 
+#[get("/")]
+async fn index() -> Result<HttpResponse, Error> {
+    let body = "
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BROKKOLI</title>
+    <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">
+    <link rel=\"shortcut icon\" type=\"image/png\" href=\"/static/favicon.ico\"/>
+    <link rel=\"stylesheet\" href=\"/static/pico.min.css\">
+  </head>
+  <body>
+    <nav class=\"container-fluid\">
+      <ul>
+        <li><a href=\"/\"><strong>BROKKOLI</strong></a></li>
+      </ul>
+    </nav>
+    <main class=\"container\">
+      <img src=\"/stream/0\">
+    </main>
+
+    <footer class=\"container-fluid\">
+    </footer>
+  </body>
+</html>
+    ";
+
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8")
+       .body(body))
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(|| {
+        let generated = generate();
         App::new()
             .app_data(web::Data::new(Client::default()))
-            .service(Files::new("/static", "./static/"))
-            .service(index)
+            .service(ResourceFiles::new("/static", generated))
             .service(proxy)
+            .service(index)
             .wrap(Logger::default())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8070))?
     .run()
     .await
 }
